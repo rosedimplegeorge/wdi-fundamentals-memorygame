838398489e61eb289fcf1548e75f88f70b054d0f
Rose  George <rosegeorge@Roses-MacBook-Pro.local>
2018-03-04 22:41:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,27 @@
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo =cards[2];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-console.log("User flipped " +cardOne);
-console.log("User flipped " +cardTwo);
-
-if(cardsInPlay.length === 2){
-	console.log("There are  " + cardsInPlay.length);
-}
-
+var cardId;
+var checkForMatch = function(){
 if(cardsInPlay[0] === cardsInPlay[1]){
 alert("You found a match!");
 }else
 {
 	alert("Sorry ! try again !");
 }
+}
+var flipCard = function(cardId){
+console.log("User flipped " +cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+/*if(cardsInPlay.length === 2){
+	console.log("There are  " + cardsInPlay.length);
+}*/
+checkForMatch();
+
+};
+
+flipCard(2);
+flipCard(0);
 
 
 
