0140941a9b1954aed4edd7d264a95767222687e8
Rose  George <rosegeorge@Roses-MacBook-Pro.local>
2018-03-05 00:12:18 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,42 @@
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank:"queen",
+suit:"hearts",
+cardImage:"https://i.imgur.com/XPknU5N.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"https://i.imgur.com/DntzTZ4.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"https://i.imgur.com/0vKNnJ0.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"https://i.imgur.com/xuIldN5.png"
+}
+];
 var cardsInPlay = [];
 var cardId;
 var checkForMatch = function(){
-if(cardsInPlay[0] === cardsInPlay[1]){
-alert("You found a match!");
-}else
-{
-	alert("Sorry ! try again !");
-}
-}
+								if(cardsInPlay[0] === cardsInPlay[1])
+								{
+									alert("You found a match!");
+								}else
+								{
+									alert("Sorry ! try again !");
+								}
+								};
 var flipCard = function(cardId){
-console.log("User flipped " +cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " +cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log("cardImageSource " + cards[cardId].cardImage);
+console.log("suit  " + cards[cardId].suit);
 /*if(cardsInPlay.length === 2){
 	console.log("There are  " + cardsInPlay.length);
 }*/
@@ -20,7 +44,7 @@ checkForMatch();
 
 };
 
-flipCard(2);
+flipCard(1);
 flipCard(0);
 
 
